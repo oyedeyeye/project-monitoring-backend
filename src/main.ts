a409b4c8eb230e640f4ed4a0b496831b6559a13d
Hezekiah Oyemade <oyedey@gmail.com>
2026-05-26 18:54:47 +0100

    DB Optimization

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+      // Allow all origins (local development, vercel preview & production domains)
+      // This echoes the request origin back to resolve browser mixed credentials issues
+      callback(null, true);
+    },
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Accept,Authorization,X-Requested-With',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('PPMIU Analytics API')
